677b83423cbed754430a175c024a958ae50d415f
Brad Coffield <bcoffield@gmail.com>
2018-11-20 13:42:58 -0700

    firebase is always such a shitshow to setup. wHY?!

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,69 @@
   <div id="app">
     <div class="left">
       <!-- <editing-test thing="hihhhhh"></editing-test> -->
-      <billings-weather v-if="showbillingsWeather"></billings-weather>
-      <chekhov-quote></chekhov-quote>
+      <billingsWeather v-if="showbillingsWeather"></billingsWeather>
+      <chekhovQuote v-if="showChekhovQuote"></chekhovQuote>
       <basho-quote></basho-quote>
       <wordnikOfDay></wordnikOfDay>
       <pinboard></pinboard>
       <xkcd></xkcd>
-      <rss-feeds v-if="showrssFeeds"></rss-feeds>
+      <rssFeeds v-if="showrssFeeds"></rssFeeds>
     </div>
     <div class="right">
       <button @click="showbillingsWeather = !showbillingsWeather">Billings Weather</button>
       <button @click="showrssFeeds = !showrssFeeds">show me feedz</button>
+      <button @click="showChekhovQuote = !showChekhovQuote">Checkhov quote</button>
+      <button @click="addData"></button>
     </div>
   </div>
 </template>
 
 <script>
+import Firebase from "firebase";
+require("firebase/firestore");
+
+var config = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  
+  authDomain: "shinytabvue.firebaseapp.com",
+  databaseURL: "https://shinytabvue.firebaseio.com",
+  projectId: "shinytabvue",
+  storageBucket: "shinytabvue.appspot.com",
+  messagingSenderId: "790342025768"
+};
+var firebaseApp = Firebase.initializeApp(config);
+const firestore = firebaseApp.firestore();
+const settings = {  timestampsInSnapshots: true };
+firestore.settings(settings);
+
 import ChekhovQuote from "./components/Quotes/Chekhov";
 import rssFeeds from "./components/rssFeeds";
 import BashoQuote from "./components/Quotes/Basho";
 import xkcd from "./components/RSS/xkcd";
 import billingsWeather from "./components/billingsWeather";
 import pinboard from "./components/Pinboard/pinboard";
-import wordnikOfDay from "./components/Wordy/wordnik-of-day"
+import wordnikOfDay from "./components/Wordy/wordnik-of-day";
 
 export default {
+  firestore: function() {
+    return {
+      persons: firestore.collection("persons")
+    };
+  },
+  methods: {
+    addData: function() {
+      this.$firestore.persons.add({
+        firstname: "Amrani",
+        lastname: "Houssain"
+      });
+    }
+  },
   data: function() {
-    return { showrssFeeds: true, showbillingsWeather: true };
+    return {
+      showrssFeeds: false,
+      showbillingsWeather: true,
+      showChekhovQuote: false
+    };
   },
   /* 
 HEY MAYBE I CAN V-FOR OVER THE BELOW AND LIKE DYNAMICALLY BUILD BUTTON COMPONENTS THAT REFER
--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
+import VueFirestore from 'vue-firestore'
+
+
+
+Vue.use(VueFirestore)
 
 Vue.prototype.$http = axios;
 
-
 Vue.config.productionTip = false
+
+
+
+
 
 new Vue({
   render: h => h(App),
