0dacf6c337e795087f60cd83704382dd2c21bc97
Brad Coffield <bcoffield@gmail.com>
2018-11-25 10:12:28 -0700

    followed buefy install instructions

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
-import VueFirestore from 'vue-firestore'
-import VueMasonry from 'vue-masonry-css'
 
-Vue.use(VueMasonry);
+
+import VueFirestore from 'vue-firestore'
 Vue.use(VueFirestore)
+
+
+import VueMasonry from 'vue-masonry-css'
+Vue.use(VueMasonry);
+
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
 
 Vue.prototype.$http = axios;
 
